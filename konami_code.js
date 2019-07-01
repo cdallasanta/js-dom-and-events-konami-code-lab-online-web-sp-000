ddac69a229b0bf47c10ed15b75cb169042ae1496
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-07-01 17:59:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const index = 0;
 
+  document.body.addEventListener('keydown', function(e) {
+    if e.
+  });
 }
-
-
-
-document.body.addEventListener('keydown', konami(e));
